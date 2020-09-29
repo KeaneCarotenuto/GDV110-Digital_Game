96e85cd217220297dd99f982784585efe0c56e7d
Keane <KeaneCarotenuto@gmail.com>
2020-09-29 17:47:14 +1300

    Re-Worked Movement
    Fixed Movement script, made more responsive, added downwards force, added coyote time, added "inverse coyote time" (time before hitting platform), added Wall Jumping, better vertical jumping, wall sliding, and some other stuff?

---
+++ Assets/GroundColliderScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundColliderScript : MonoBehaviour
+{
+    public PlayerMovement pmScript;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        pmScript.IsGrounded = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        pmScript.IsGrounded = true;
+        pmScript.IsJumping = false;
+        pmScript.JumpTime = pmScript.defaultJumpTime;
+        pmScript.leavesGroundTime = Time.time;
+    }
+}
---
+++ Assets/WallColliderScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallColliderScript : MonoBehaviour
+{
+    public PlayerMovement pmScript;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        pmScript.onWall = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        pmScript.onWall = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        pmScript.onWall = true;
+    }
+}
