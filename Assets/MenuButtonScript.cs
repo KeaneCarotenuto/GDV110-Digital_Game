343c50f3c5b850cf4fb957459de1ce09279fb451
Keane <KeaneCarotenuto@gmail.com>
2020-10-27 16:19:17 +1300

    Hooked Up Start Button to Start Game

---
+++ Assets/MenuButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonScript : MonoBehaviour
+{
+    public string Level1Name;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(Level1Name, LoadSceneMode.Single);
+    }
+}
